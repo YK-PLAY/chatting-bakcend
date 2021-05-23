bf36cef7a4219f8d8d9409fc926be2c1a751a659
Kebron <ztkmkoo@gmail.com>
2021-05-23 18:41:37 +0900

    YCA-13 make rest api server run (#2)
    * YCA-13 make rest api server run

---
+++ src/main/kotlin/com/github/ykplay/test/models/UserAgeResponse.kt
@@ -0,0 +1,10 @@
+package com.github.ykplay.test.models
+
+import kotlinx.serialization.Serializable
+
+/**
+ * @author Kebron ztkmkoo@gmail.com
+ * @create 2021-05-23 17:47
+ */
+@Serializable
+data class UserAgeResponse(val age: Int)
